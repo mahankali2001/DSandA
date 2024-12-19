046bfb5830e512359e68b5338733c49f7ecbddc6
mahankali <vijayakumar.mahankali@gmail.com>
2024-12-19 00:33:22 -0800

    multithreading with specific example

--- MultiThreading.java
+++ MultiThreading.java
@@ -1,24 +1,28 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MultiThreading {
-
     public static void main(String[] args) {
-        MTAMain mtaMain = new MTAMain();
+        // MTAMain spans MTAChild threads using thread pooling
+        MTAMain mtaMain = new MTAMain(2);
         mtaMain.process();
     }
 
     static class MTAMain  {
-        // Create a thread pool with 5 threads
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        // Thread pooling
+        ExecutorService executorService = null;
 
-        public MTAMain () {
+        public MTAMain (int MTAChildThreadCount) {
+            executorService = Executors.newFixedThreadPool(MTAChildThreadCount);
         }
 
         public void process() {
-            // Check eligible deliveries & personalizataion info
-            // create all delivery parts and span 5 mtachild threads to process each part
-            for (int i = 0; i < 10; i++) {
+            // Check & get eligible delivery templates & delivery segment personalizataion info
+            // create delivery part xml files with all needed data
+            // Span MTAChild threads to process delivery parts
+            for (int i = 0; i < 4; i++) {
                 executorService.submit(new MTAChild(i));
             }
 
@@ -28,17 +32,41 @@ public class MultiThreading {
     }
 
     static class MTAChild implements Runnable {
+        private Lock lock = new ReentrantLock();
+        private int counter = 0;
         private int taskId;
 
         public MTAChild(int taskId) {
             this.taskId = taskId;
         }
 
+        /**
+         * MTA child create batch of emails/Push msgs from each delivery part send it to mail sending service
+         * */ 
         @Override
         public void run() {
             System.out.println("Task ID : " + this.taskId + " performed by " 
                 + Thread.currentThread().getName());
+
+            // Thread synchronization
+            lock.lock();
+            try {
+                // shared resource
+                counter++;
+
+                System.out.println("Counter: " + counter + " updated by " + Thread.currentThread().getName());
+                Thread.sleep(2000);
+            } 
+             catch (InterruptedException e) {
+                e.printStackTrace();
+            } 
+            finally{
+                lock.unlock();
+            }
         }
     }
     
 }
+// Thread safety is achieved by using the ReentrantLock class. The lock() method is used to acquire the lock, and the unlock() method is used to release the lock. This ensures that only one thread can access the shared resource at a time, preventing
+// Deadlocs - two or more threads are waiting for each other to release the lock, causing all threads to be blocked indefinitely. This can be avoided by ensuring that the lock is released after it is acquired, as shown in the example
+// Performance - Locks can be more expensive than synchronized blocks, as they involve more overhead. However, they provide more flexibility and control over the locking mechanism.
